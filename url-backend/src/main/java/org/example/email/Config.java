7058e81567649ef9b4ed09b3625ac177e2a199c2
Aditya <Adityasingh60908@gmail.com>
2026-07-04 03:29:25 +0530

    Security file fixed

--- url-backend/src/main/java/org/example/email/Config.java
+++ url-backend/src/main/java/org/example/email/Config.java
@@ -1,13 +1,15 @@
 package org.example.email;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ComponentScan("org.example.email")
+@Configuration
 public class Config {
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
